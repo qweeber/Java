13d0179f7ff38981f69580c78d59bea3225cffd9
Qweeber <kamil-45.3@wp.pl>
2020-04-27 18:29:25 +0200

    Create test.txt by File class

--- src/OperationsStream/FilesDirectories/Main.java
+++ src/OperationsStream/FilesDirectories/Main.java
@@ -1,7 +1,21 @@
 package OperationsStream.FilesDirectories;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        try{
+            File file = new File("test.txt");
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
