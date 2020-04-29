5ea6d85e67dab557a9fba26d11fba4f865f15bfb
Qweeber <kamil-45.3@wp.pl>
2020-04-29 19:21:12 +0200

    Add write and read.

---
+++ src/OperationsStream/WriteRead/Main.java
@@ -0,0 +1,37 @@
+package OperationsStream.WriteRead;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try{
+            PrintWriter printWriter = new PrintWriter(new FileWriter("date.txt"));
+
+            printWriter.println(3245);
+            //printWriter.flush();
+            printWriter.close();
+
+            String j = "cm";
+            printWriter = new PrintWriter(new FileWriter("date.txt", true));
+            printWriter.append("lala");
+            printWriter.println();
+            printWriter.printf("He weighs %d kg and has %.2f %s height.", 50, 196.0, j);
+
+            printWriter.close();
+
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        Main.test(4,8,9, "dsf", "sdf", 99);
+
+    }
+
+    static void test(Object... a){
+        for(int i=0; i<a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+
+}
